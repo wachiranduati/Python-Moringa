3f62d79faa418b757c7ea0ef7dab60b447c11a1d
Wachira Nduati <wachiranduati@gmail.com>
2022-04-07 15:05:29 +0300

    create write function on jsonengine

--- tests/test_json_engine.py
+++ tests/test_json_engine.py
@@ -7,6 +7,8 @@ class TestJson(unittest.TestCase):
     
     path = pathlib.Path("tests/data/dataset_test.json")
     x =  JsonEngine()
+    data = [{"id":1,"first_name":"Yvor","last_name":"McNalley","email":"ymcnalley0@angelfire.com","gender":"Male","ip_address":"160.92.169.74"},
+    {"id":2,"first_name":"Amber","last_name":"Loudyan","email":"aloudyan1@google.com.hk","gender":"Female","ip_address":"12.206.65.11"},]
 
     def setUp(self) -> None:
         self.x.setupTestFile(0)
@@ -16,10 +18,11 @@ class TestJson(unittest.TestCase):
         self.assertEqual((str(self.path), self.path.is_file()), (str(self.path), True))
 
     def test_write_data(self):
+        self.x.writeRow(self.data, 0)
         self.assertTrue(False)    
 
     def tearDown(self) -> None:
-        self.x.deleteFile()
+        # self.x.deleteFile()
         return super().tearDown()
 
 if __name__ == '__main__':
